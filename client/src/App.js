2900a52fcaa95eab27263f1933430ad81aff3d43
Anjo Tadena <tadena.anjo@gmail.com>
2020-05-03 22:09:58 +0800

    Basic API call implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://localhost:8081/')
+      .then((response) => {
+        console.log('FROM API', response);
+      });
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload try.
         </p>
         <a
           className="App-link"
